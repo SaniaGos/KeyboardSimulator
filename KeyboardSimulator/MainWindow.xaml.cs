cf43c23dc9507d54fcd7b6e577313ed9297cbfcf
SaniaGos <saniagoss@gmail.com>
2021-05-17 23:50:34 +0300

    new

---
+++ KeyboardSimulator/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyboardSimulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            foreach (var items in OwnerPenal.Children)
+            {
+                if (items is StackPanel)
+                {
+                    foreach (var item in (items as StackPanel).Children)
+                    {
+                        if (item is Button)
+                        {
+                            string key = e.Key.ToString();
+                            string button = ((item as Button).Content as string);
+                            bool rez = key.Contains(button);
+                            if (rez)
+                            {
+                                (item as Button).IsEnabled = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
