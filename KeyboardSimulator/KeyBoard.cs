36c67876793ef7bcada8b8d5361349c40d8a517b
SaniaGos <saniagoss@gmail.com>
2021-05-18 21:57:35 +0300

    ShiftWorks_V0.5

---
+++ KeyboardSimulator/KeyBoard.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KeyboardSimulator
+{
+    enum Language
+    {
+        English, Ukrainian
+    }
+    class KeyBoard
+    {
+        public List<Button> Buttons { get; set; }
+        public Language Language { get; set; }
+
+        public KeyBoard()
+        {
+            Buttons = new List<Button>();
+            Language = Language.English;
+        }
+        static string[] smallKeyNameEn = new string[]
+        {
+            "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "BackSpace",
+            "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\",
+             "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter",
+              "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "Shift",
+               "Ctrl", "Win", "Alt", "Space", "Alt", "Win", "Ctrl"
+        };
+        static string[] bigKeyNameEn = new string[]
+        {
+            "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "BackSpace",
+            "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "|",
+             "CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "\"", "Enter",
+              "Shift", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?", "Shift",
+               "Ctrl", "Win", "Alt", "Space", "Alt", "Win", "Ctrl"
+        };
+        static string[] smallKeyNameUa = new string[]
+        {
+            "ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "BackSpace",
+            "Tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ї", "\\",
+             "CapsLock", "ф", "і", "в", "а", "п", "р", "о", "л", "д", "ж", "є", "Enter",
+              "Shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".", "Shift",
+               "Ctrl", "Win", "Alt", "Space", "Alt", "Win", "Ctrl"
+        };
+
+        public void ButtonDefaultContent()
+        {
+            if(Buttons.Count == smallKeyNameEn.Length)
+            {
+                for (int i = 0; i < smallKeyNameEn.Length; i++)
+                {
+                    if(Language == Language.English)
+                        Buttons[i].Content = smallKeyNameEn[i];
+                    else Buttons[i].Content = smallKeyNameUa[i];
+                }
+            }
+        }
+        public void ButtonShiftContent()
+        {
+            if (Buttons.Count == smallKeyNameEn.Length)
+            {
+                for (int i = 0; i < smallKeyNameEn.Length; i++)
+                {
+                    if (Language == Language.English)
+                        Buttons[i].Content = bigKeyNameEn[i];
+                    else Buttons[i].Content = smallKeyNameUa[i];
+                }
+            }
+        }
+
+    }
+}
--- KeyboardSimulator/MainWindow.xaml.cs
+++ KeyboardSimulator/MainWindow.xaml.cs
@@ -22,14 +22,50 @@ namespace KeyboardSimulator
     /// </summary>
     public partial class MainWindow : Window
     {
+        KeyBoard board;
         public MainWindow()
         {
             InitializeComponent();
-
-            Icon = Properties.Resources.Keyboard;
+            board = new KeyBoard();
+            FillButtonList();
+            ButtonsContent();
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            foreach (Button button in board.Buttons)
+                if (e.Key.ToString().Equals(button.Name) || e.SystemKey.ToString().Equals(button.Name))
+                {
+                    button.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Primitives.ButtonBase.ClickEvent));
+                }
+        }
+        
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+        
+        private async Task UpdateButtonVisualState(Button button)
+        {
+            typeof(Button).GetMethod("set_IsPressed", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(button, new object[] { true });
+            await Task.Delay(TimeSpan.FromMilliseconds(500));
+            typeof(Button).GetMethod("set_IsPressed", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(button, new object[] { false });
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _ = UpdateButtonVisualState(sender as Button);
+
+            if (e.Source is Button)
+            {
+                string key = (e.Source as Button).Content.ToString();
+                if (key == "Space") key = " ";
+                if (key == "Shift") ButtonsShiftContent();
+                if (key.Length == 1)
+                    WriteText.Children.Add(new ListBoxItem() { Padding = new Thickness(0), FontSize = 25, Background = Brushes.LightGreen, Content = key });
+
+            }
+        }
+        private void FillButtonList()
         {
             foreach (var items in OwnerPenal.Children)
             {
@@ -39,28 +75,21 @@ namespace KeyboardSimulator
                     {
                         if (item is Button)
                         {
-                            Button button = (item as Button);
-                            
-                            if (e.Key.ToString().Equals(button.Name) || e.SystemKey.ToString().Equals(button.Name))
-                            {
-                                button.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Primitives.ButtonBase.ClickEvent));
-                            }
-
+                            board.Buttons.Add(item as Button);
                         }
                     }
                 }
             }
         }
+        private void ButtonsContent()
+        {
+            board.ButtonDefaultContent();
+        }
+        private void ButtonsShiftContent()
+        {
+            board.ButtonShiftContent();
+        }
 
-        private async Task UpdateButtonVisualState(Button button)
-        {
-            typeof(Button).GetMethod("set_IsPressed", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(button, new object[] { true });
-            await Task.Delay(TimeSpan.FromMilliseconds(200));
-            typeof(Button).GetMethod("set_IsPressed", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(button, new object[] { false });
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            _ = UpdateButtonVisualState(sender as Button);
-        }
+        
     }
 }
